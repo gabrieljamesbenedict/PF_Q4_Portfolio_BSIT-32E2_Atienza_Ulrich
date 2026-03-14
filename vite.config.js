d5820450f08189e6e3dddf3d8cc39a81d6f6fedb
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-15 01:04:19 +0800

    change base path for github page compat

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/', // <- dynamic
+  //base: process.env.VITE_BASE_URL || '/', // <- dynamic,
+  base: "/pf-q4-portfolio-bsit-32e2-atienza-ulrich/",
+  build: {
+    outDir: 'docs'
+  }
 });
