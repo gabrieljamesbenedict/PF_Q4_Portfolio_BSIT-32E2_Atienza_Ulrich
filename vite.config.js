cb7d1316634647381b06dae5b4a57673abffec62
Uwich <ulrich.atienza@gmail.com>
2026-03-14 23:18:47 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react()],
+  // BASE PATH:
+  // '/' → root domain
+  // '/repo-name/' → GitHub Pages repository
+  base: process.env.VITE_BASE_URL || '/', 
+});
