7822d05e5d57d6f20d3353d8934f6a0bc4b3a8a5
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-15 09:09:47 +0800

    change base path to "./"

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   //base: process.env.VITE_BASE_URL || '/', // <- dynamic,
-  base: "/pf-q4-portfolio-bsit-32e2-atienza-ulrich/",
+  base: "./",
   build: {
     outDir: 'docs'
   }
